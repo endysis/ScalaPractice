15bdfadd1d16947527b1dee496f329fcb5759709
douglaso <osheadouglas@rentalcars.com>
2019-11-15 17:17:31 +0000

    Vals and vars

--- src/lectures/part1basics/ValuesCariablesTypes.scala
+++ src/lectures/part1basics/ValuesCariablesTypes.scala
@@ -7,7 +7,27 @@ object ValuesVariablesTypes extends App {
 
   // VALS ARE IMMUTABLE
 
+  val aString: String = "heythere"
+  val anotherString = "goodbye"
 
-  val aString: String - "heythere"
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 76475854749373L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 4.23
+
+  // variables
+  var aVariable : Int = 4
+
+  aVariable = 5 // Side effects here
+
+//  prefer vals over vars
+//  all vals and vars have types
+//  compiler automatically infers types when omitted
 
 }
+
+
+// https://booking.udemy.com/course/rock-the-jvm-scala-for-beginners/learn/lecture/7660550#overview
